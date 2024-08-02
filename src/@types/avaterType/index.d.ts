09336bf5dc12db1b58534299ffd5fe2ba1d73958
Kosei805 <na913420ka85@gmail.com>
2024-08-02 15:04:00 +0900

    Add avater data

---
+++ src/@types/avaterType/index.d.ts
@@ -0,0 +1,17 @@
+export interface AvaterType {
+  name: string
+  src: string
+  shortSetting: string
+  avaterPrompt?: AvaterPromptType
+}
+
+interface AvaterPromptType {
+  age: number
+  conservationSetting: ConservationSettingType
+  
+}
+
+interface ConservationSettingType {
+  features: string[]
+  example: string[]
+}
